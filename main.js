3bfcafeb2cadce41153333405ff11cbbb86d508d
aryansathishkumar <76153434+aryansathishkumar@users.noreply.github.com>
2021-07-13 14:22:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function preload()
+{
+
+}
+
+function setup() {
+  canvas = createCanvas(250, 250);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  video.center()
+  classifier = ml5.imageClassifier("MobileNet",modelloaded);
+}
+
+function draw()
+{
+  image(video, 0, 0, 250 ,250);
+
+  setTimeout(function() {
+    classifier.classify(video, getresult); 
+  }, 3500);
+}
+
+function modelloaded()
+{
+  console.log(ml5.version,"model is loaded");
+}
+
+function getresult(error, result)
+{
+  if(error)
+  {
+    console.error(error);
+  }
+  else
+  {
+    console.log(result);
+    
+    document.getElementById("object2").innerHTML = result[0].label;
+    document.getElementById("accuracy2").innerHTML = result[0].confidence.toFixed(2)*100 + "%";
+  }
+}
+
+
+
+
+
+
